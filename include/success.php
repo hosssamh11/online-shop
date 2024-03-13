7ade04fbab42b414a6a47be9c706d0fcfe1b9e1f
hosssamh11 <140279369+hosssamh11@users.noreply.github.com>
2024-03-13 22:37:45 +0200

    Delete include directory

--- include/success.php
+++
@@ -1,5 +0,0 @@
-<?php
-if (isset($_SESSION['success'])) {
-    echo '<div class="alert alert-success" role="alert">' . $_SESSION['success'] . '</div>';
-    unset($_SESSION['success']); // Clear the success message
-}
